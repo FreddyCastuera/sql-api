f86ff9282b6e4430cc056f016ce90be88a781cd7
freddy Castuera <freddycastuera@gmail.com>
2021-12-25 05:25:02 -0700

    agregamos variables de entorno

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
 require('dotenv').config()
-
+//hola
 
 const connection = mysql.createConnection({
     host:process.env.DB_HOST,
